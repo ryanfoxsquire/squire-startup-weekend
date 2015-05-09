5a50ab2c1ca26d90349d097a7a439de1950eb0d0
Ryan Fox Squire <ryan.fox.squire@gmail.com>
2015-05-08 22:52:45 -0700

    refactored so that this will work on local and production without annoying changes git status

--- core/data/catseverywhere.py
+++ core/data/catseverywhere.py
@@ -24,5 +24,6 @@ def emails():
     return render_template('emails.html', email_addresses=email_addresses)
 
 if __name__ == "__main__":
+
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
