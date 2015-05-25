be2f9823f4f2b453eb5d36adb5d4ff0168b8f1ba
Ryan Fox Squire <ryan.fox.squire@gmail.com>
2015-05-24 18:50:06 -0700

    PART 3 complete for realpython tutorial, see /realpythontutorial_index

--- catseverywhere.py
+++ catseverywhere.py
@@ -5,6 +5,16 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from settings import APP_STATIC
 import csv
 from pdb import set_trace as pause
+import requests
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
+import operator
+import os
+import requests
+import re
+import nltk
+
 app = Flask(__name__)
 
 # Set Config Settings
@@ -59,6 +69,64 @@ def authors():
 def emails():
     # we should remove this route. 
     return render_template('emails.html', email_addresses=email_addresses)
+
+@app.route('/realpythontutorial_index', methods=['GET', 'POST'])
+def realpythontutorial_index():
+    # from https://realpython.com/blog/python/flask-by-example-part-3-text-processing-with-requests-beautifulsoup-nltk/
+
+    errors = []
+    results = {}
+    if request.method == "POST":
+        print("POST RECEIVED\n\n")
+        # get url that the person has entered
+        try:
+            url = request.form['url']
+            print("\n")
+            print(url)
+
+            r = requests.get(url)
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+            return render_template('realpythontutorial_index.html', errors=errors)
+        if r:
+            print("detected the url!!")
+
+            # text processing
+            raw = BeautifulSoup(r.text).get_text()
+            nltk.data.path.append('./nltk_data/')  # set the path
+            tokens = nltk.word_tokenize(raw)
+            text = nltk.Text(tokens)
+
+            # remove punctuation, count raw words
+            nonPunct = re.compile('.*[A-Za-z].*')
+            raw_words = [w for w in text if nonPunct.match(w)]
+            raw_word_count = Counter(raw_words)
+
+            # stop words
+            no_stop_words = [w for w in raw_words if w.lower() not in stops]
+            no_stop_words_count = Counter(no_stop_words)
+
+            # save the results
+            results = sorted(
+                no_stop_words_count.items(),
+                key=operator.itemgetter(1),
+                reverse=True
+            )[0:10]
+            try:
+                result = Result(
+                    url=url,
+                    result_all=raw_word_count,
+                    result_no_stop_words=no_stop_words_count
+                )
+                db.session.add(result)
+                db.session.commit()
+                print("saved {0} to db!".format(url))
+            except:
+                errors.append("Unable to add item to database.")
+    print("rendering template...")
+    return render_template('realpythontutorial_index.html', errors=errors, results=results)
 
 if __name__ == "__main__":
 
